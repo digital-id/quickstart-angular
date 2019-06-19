c18adcec0a3733d459a38287b3ce7a17344c52f7
Karl Balemi <km_balemi@hotmail.com>
2019-06-19 22:25:22 +1000

    Initial commit

---
+++ src/app/verify/verify.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verify',
+  templateUrl: './verify.component.html',
+  styleUrls: ['./verify.component.css']
+})
+export class VerifyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+    const script = document.createElement('script');
+
+    script.src = 'https://digitalid-sandbox.com/sdk/app.js';
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    script.onload = () => {
+
+      /* Verify with Digital iD */
+      window.digitalId.init({
+        clientId: '[YOUR CLIENT ID]',
+        uxMode: 'popup',
+        onLoadComplete() {
+        },
+        onComplete(msg) {
+
+        },
+        onClick(opts) {
+        },
+        onKeepAlive() {
+        }
+      });
+    };
+  }
+}
